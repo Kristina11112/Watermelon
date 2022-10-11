719320a37abdf23d8166793ce2d30c4a89459c6a
Kristina11112 <115408741+Kristina11112@users.noreply.github.com>
2022-10-11 15:37:22 +0300

    Add files via upload

---
+++ Watermelon.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	int a;
+	std::cout << "Input number";
+	std::cin >> a;
+
+	if (a % 2 == 0 && (a / 2) % 2 == 0)
+	{
+		std::cout << "Yes";
+	}
+	else
+	{
+		std::cout << "No";
+	}
+	
+	std::cout << "Goodbye!";
+}
